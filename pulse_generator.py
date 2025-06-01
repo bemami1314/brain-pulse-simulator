67f7d487f3719576e5a98af4d45ccb76979f0473
bemami_nia <bemami1314@gmail.com>
2025-06-02 01:52:52 +0330

    Update pulse_generator.py

--- pulse_generator.py
+++ pulse_generator.py
@@ -1,5 +1,6 @@
 import numpy as np
 import json
+import matplotlib.pyplot as plt
 
 n = 100
 
@@ -11,7 +12,19 @@ for _ in range(n - 1):
     new_pulse = round(np.clip(prev + change, 0, 1), 6)
     pulses.append(new_pulse)
 
-
+# ذخیره پالس‌ها در فایل JSON
 with open("simulated_brain_pulses.json", "w") as f:
     json.dump(pulses, f, indent=2)
+
+# رسم نمودار
+time = np.linspace(0, 1, n)
+plt.plot(time, pulses, color='royalblue')
+plt.title("Simulated Brain Pulses")
+plt.xlabel("Time (s)")
+plt.ylabel("Pulse Value")
+plt.grid(True)
+
+# ذخیره نمودار
+plt.savefig("pulse_plot.png")
+plt.close()
 
