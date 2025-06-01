7e98585700bca59e20c610ee79ebee7ca6b58387
bemami_nia <bemami1314@gmail.com>
2025-06-02 01:02:10 +0330

    Update pulse_generator.py

--- pulse_generator.py
+++ pulse_generator.py
@@ -11,7 +11,7 @@ for _ in range(n - 1):
     new_pulse = round(np.clip(prev + change, 0, 1), 6)
     pulses.append(new_pulse)
 
-# ذخیره به صورت JSON معتبر و مرتب
+
 with open("simulated_brain_pulses.json", "w") as f:
     json.dump(pulses, f, indent=2)
 
